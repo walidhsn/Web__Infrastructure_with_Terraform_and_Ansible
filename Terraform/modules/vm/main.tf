3bf7f1ecd657b9c4674d36bb3d58581a3e850fce
walidhsn <walid.benhassouna@gmail.com>
2025-03-26 16:08:29 +0100

    v0.3 - image version fix to ubuntu-24_04-lts

--- Terraform/modules/vm/main.tf
+++ Terraform/modules/vm/main.tf
@@ -57,8 +57,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 }
